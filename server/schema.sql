004e9685d2a87ec75d6da2397d9280747593bad0
Hack Reactor Student <student@hackreactor.com>
2017-09-11 16:38:09 -0500

    initial commit. created message, user, room tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,30 @@
+/*DROP DATABASE chat;*/
+
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int primary key auto_increment,
+  name varchar(50),
+  message varchar(140)
+
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id int primary key auto_increment,
+  username varchar(50)
+);
 
+CREATE TABLE rooms (
+  id int primary key auto_increment,
+  roomname varchar(50)
+);
+/* Create other tables and define schemas for them here! */
 
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
